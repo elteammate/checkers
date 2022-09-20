20d2539c3793c49c7adfc4caa95a72aa132ca592
elteammate <elteammate@gmail.com>
2022-09-20 21:04:41 +0300

    Initial commit

---
+++ Checkers/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Checkers.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
