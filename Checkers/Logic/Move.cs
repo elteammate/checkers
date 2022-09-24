25af4461ab0dec94a2834a190ab37ec70556267e
elteammate <elteammate@gmail.com>
2022-09-24 19:15:19 +0300

    Document and refactor Move

--- Checkers/Logic/Move.cs
+++ Checkers/Logic/Move.cs
@@ -1,3 +1,10 @@
 namespace Checkers.Logic;
 
-public record Move(Color Color, Position From, Position To, Position? Eaten = null);
+/// <summary>
+/// Represents a move in the game.
+/// </summary>
+/// <param name="Color">A player who made that move</param>
+/// <param name="From">A starting position of the piece</param>
+/// <param name="To">A position where piece ended up</param>
+/// <param name="Jumped">If not null, a position of a piece jumped over</param>
+public record Move(Color Color, Position From, Position To, Position? Jumped = null);
