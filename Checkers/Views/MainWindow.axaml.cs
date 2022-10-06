8a877e82f62b41b1cc311d636e9e9938a9e28dfe
elteammate <elteammate@gmail.com>
2022-10-06 12:45:22 +0300

    Add comment about why CanResize does not work

--- Checkers/Views/MainWindow.axaml.cs
+++ Checkers/Views/MainWindow.axaml.cs
@@ -2,6 +2,9 @@ using Avalonia.Controls;
 
 namespace Checkers.Views;
 
+// Resize issue
+// https://github.com/AvaloniaUI/Avalonia/issues/8329
+
 public partial class MainWindow : Window
 {
     public MainWindow()
